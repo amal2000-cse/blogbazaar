938fcf83d0cdcd0e05baf5252d3bde176f27635d
amal2000-cse <kannothparambathamalmanoj@gmail.com>
2024-03-29 12:31:36 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 const App = () => {
   return (
-    <div className='text-3xl text-red-500'>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="*" element={<h1>404 Page Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;

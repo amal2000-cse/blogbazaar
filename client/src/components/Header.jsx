ba9db6b4be36e1d7baa52efec4a1ab459bd4f430
amal2000-cse <kannothparambathamalmanoj@gmail.com>
2024-04-02 22:32:00 +0530

    complete dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice";
+
 const Header = () => {
-    
-    //with this we will be able to get the current path
-    const path = useLocation().pathname;
+  //with this we will be able to get the current path
+  const path = useLocation().pathname;
+  const dispatch = useDispatch();
+
+  const { currentUser } = useSelector((state) => state.user);
+  //here we want to know which theme we are using
+  const {theme } = useSelector((state) => state.theme);
+
   return (
     <Navbar className="border-b-2">
       <Link
@@ -33,31 +41,55 @@ const Header = () => {
       </Button>
 
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-          <FaMoon />
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill onClick={()=>dispatch(toggleTheme())}>
+          {theme ==='light' ?  <FaSun/> :  <FaMoon/>}
         </Button>
 
-        <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            //to replace it with the avatar we use inline
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign Out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button className="w-12 h-10" color="gray" pill>
+              Sign In
+            </Button>
+          </Link>
+        )}
 
-        <Navbar.Toggle/>
-
+        <Navbar.Toggle />
       </div>
-        <Navbar.Collapse>
-          <Navbar.Link active={path==='/'} as={'div'}>
-            <Link to="/">Home</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path==='/about'} as={'div'}>
-            <Link to="/about">About</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path==='/projects'} as={'div'}>
-            <Link to="/projects">Projects</Link>
-          </Navbar.Link>
-          {/* <Navbar.Link>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+        {/* <Navbar.Link>
                 <Link to='/blog'>Blog</Link>
             </Navbar.Link> */}
-        </Navbar.Collapse>
+      </Navbar.Collapse>
     </Navbar>
   );
 };

9444085c7c8a829ac1ecbce72234e04e3f362399
amal2000-cse <kannothparambathamalmanoj@gmail.com>
2024-04-03 10:35:20 +0530

    complete profile ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
@@ -20,7 +20,9 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
         <Route path="*" element={<h1>404 Page Not Found</h1>} />
       </Routes>

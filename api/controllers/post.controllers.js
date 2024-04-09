fc9e4ac538ef6e747a3e3b283f4857fca8e51d59
amal2000-cse <kannothparambathamalmanoj@gmail.com>
2024-04-09 13:04:47 +0530

    create get post api routes

--- api/controllers/post.controllers.js
+++ api/controllers/post.controllers.js
@@ -1,29 +1,86 @@
 import Post from "../models/post.model.js";
 import { errorHandler } from "../utils/Error.js";
 
-export const create = async(req,res, next)=>{
+export const create = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not authorized to create a post"));
+  }
 
-    if(!req.user.isAdmin){
-        return next(errorHandler(403, "You are not authorized to create a post"))
-    }
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "Please provide all the required fields"));
+  }
 
-    if(!req.body.title || !req.body.content){
-        return next(errorHandler(400, "Please provide all the required fields"))
-    }
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, "");
 
-    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, '');
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
 
-    const newPost = new Post({
-        ...req.body, slug ,userId: req.user.id
-    })
+  try {
+    const savePost = await newPost.save();
+    res.status(201).json(savePost);
+  } catch (error) {
+    next(error);
+  }
+};
 
-    try {
-        const savePost = await newPost.save();
-        res.status(201).json(savePost)
-        
-    } catch (error) {
-        next(error)
-        
-    }
+export const getPosts = async (req, res, next) => {
+  try {
+    //here we are mentioning the start and end index of the posts
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    //in mongoDB 1 will show ascending and -1 will show descending
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      //if the search query has category
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { category: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
 
-}
+      //we want to search for both the title and the content of the posts
+      ...(req.query.searchTerm && {
+        $or: [
+          //regex is going to search inside the title
+          //$options : i, if its lowers case of uppercase it doesnt matter
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      })
+  }).sort({updatedAt: sortDirection}).skip(startIndex).limit(limit);
+
+  //i also want to get the total number of posts also
+  const totalPost = await Post.countDocuments();
+
+  //total number of posts in last month
+
+  const now = new Date();
+  const oneMonthAgo = new Date(
+    now.getFullYear(),
+    now.getMonth() - 1,
+    now.getDate()
+  );
+
+  const lastMonthPosts = await Post.countDocuments({
+    createdAt: {
+      $gte: oneMonthAgo,
+    },
+  })
+
+  res.status(200).json({
+    posts,
+    totalPost,
+    lastMonthPosts,
+  });
+
+
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create } from '../controllers/post.controllers.js';
+import { create, getPosts } from '../controllers/post.controllers.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create);
+//for the getposts routes, we are making it in such a way that, even
+//the normal user other than the admin will be able to get the posts,
+//and also we will be able to get the posts by searching also, using the search bar
+//so this route will be tricky
+//so we wont be veriying the user in this route
+router.get('/getposts', getPosts)
 
 export default  router;

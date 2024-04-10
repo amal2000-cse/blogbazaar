f9f524080c3b5582863f78b1b09d86a49e9fbda4
amal2000-cse <kannothparambathamalmanoj@gmail.com>
2024-04-10 11:26:36 +0530

    create getUsers api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -70,9 +70,7 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
-
 export const deleteUser = async (req, res, next) => {
-
   //before the user reaches here , we want to make sure, if the user is signed in or not
   if (req.user.id !== req.params.userId) {
     return next(
@@ -82,20 +80,55 @@ export const deleteUser = async (req, res, next) => {
 
   try {
     await User.findByIdAndDelete(req.params.userId);
-    res.status(200).json('User deleted successfully');
-  } catch (error) {
-    
-  }
-}
+    res.status(200).json("User deleted successfully");
+  } catch (error) {}
+};
 
-export const signOut =(req,res,next)=>{
+export const signOut = (req, res, next) => {
   try {
-    res.clearCookie('access_token').status(200).json('User has been signed out');
-    
+    res
+      .clearCookie("access_token")
+      .status(200)
+      .json("User has been signed out");
   } catch (error) {
     next(error);
-    
   }
+};
 
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not authorized to see all users"));
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex || 0);
+    const limit = parseInt(req.query.limit || 9);
+    const sortDirection = req.query.sort || "asc" ? 1 : -1;
 
-}
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUsers = await User.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: {
+        $gte: oneMonthAgo,
+      },
+    });
+
+    res.status(200).json({ usersWithoutPassword, totalUsers, lastMonthUsers });
+  } catch (error) {
+    next(error);
+  }
+};

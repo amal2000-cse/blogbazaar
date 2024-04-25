bccfae2548105e6c75fe98f79022fa4de3797a81
amal2000-cse <kannothparambathamalmanoj@gmail.com>
2024-04-25 22:28:03 +0530

    show the comments of a post

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,22 +1,23 @@
-import { Alert, Button, TextInput, Textarea } from "flowbite-react";
-import React, { useState } from "react";
+import { Alert, Button, Modal, TextInput, Textarea } from "flowbite-react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Comment from "./Comment";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 
-const CommentSection = ({ postId }) => {
-  //if the user is not signed in, we need to show, u have to login to comment on this post
-
+export default function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState("");
   const [commentError, setCommentError] = useState(null);
-
+  const [comments, setComments] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [commentToDelete, setCommentToDelete] = useState(null);
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     if (comment.length > 200) {
       return;
     }
-
     try {
       const res = await fetch("/api/comment/create", {
         method: "POST",
@@ -33,13 +34,29 @@ const CommentSection = ({ postId }) => {
       if (res.ok) {
         setComment("");
         setCommentError(null);
-        console.log(data);
+        setComments([data, ...comments]);
       }
     } catch (error) {
-      setCommentError(error);
+      setCommentError(error.message);
     }
   };
 
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const res = await fetch(`/api/comment/getPostComments/${postId}`);
+        if (res.ok) {
+          const data = await res.json();
+          setComments(data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getComments();
+  }, [postId]);
+
+ 
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
@@ -51,8 +68,8 @@ const CommentSection = ({ postId }) => {
             alt=""
           />
           <Link
-            className="text-xs text-cyan-600 hover:underline"
             to={"/dashboard?tab=profile"}
+            className="text-xs text-cyan-600 hover:underline"
           >
             @{currentUser.username}
           </Link>
@@ -61,24 +78,22 @@ const CommentSection = ({ postId }) => {
         <div className="text-sm text-teal-500 my-5 flex gap-1">
           You must be signed in to comment.
           <Link className="text-blue-500 hover:underline" to={"/sign-in"}>
-            Sign in
+            Sign In
           </Link>
         </div>
       )}
-
       {currentUser && (
         <form
           onSubmit={handleSubmit}
           className="border border-teal-500 rounded-md p-3"
         >
           <Textarea
-            onChange={(e) => setComment(e.target.value)}
             placeholder="Add a comment..."
             rows="3"
             maxLength="200"
+            onChange={(e) => setComment(e.target.value)}
             value={comment}
           />
-
           <div className="flex justify-between items-center mt-5">
             <p className="text-gray-500 text-xs">
               {200 - comment.length} characters remaining
@@ -94,8 +109,21 @@ const CommentSection = ({ postId }) => {
           )}
         </form>
       )}
+      {comments.length === 0 ? (
+        <p className="text-sm my-5">No comments yet!</p>
+      ) : (
+        <>
+          <div className="text-sm my-5 flex items-center gap-1">
+            <p>Comments</p>
+            <div className="border border-gray-400 py-1 px-2 rounded-sm">
+              <p>{comments.length}</p>
+            </div>
+          </div>
+          {comments.map((comment) => (
+            <Comment key={comment._id} comment={comment} />
+          ))}
+        </>
+      )}
     </div>
   );
-};
-
-export default CommentSection;
+}
